08d779fa5dd15d17aba78a27ece347a633180ddb
Bryant Caballero <Brayanmc.contacto@gmail.com>
2022-06-24 00:45:41 -0400

    👀 Feature: The algorithm that handles errors has been changed to keep it more orderly

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,43 +1,88 @@
+
+// This is where I handle every errors
+const ERRORS = {
+    'user not found': {
+        message: 'Oops, it seems that there is no user with such information 😞',
+        status: 404
+    },
+    
+    'the user already exist': {
+        message: 'Try to put a different password or email 😔',
+        status: 409
+    },
+
+    'info doesnt meet the requirements': {
+        message: 'The information passed does not meet the minimum accepted requirements 😕',
+        status: 401
+    },
+
+    'user wasnt created': {
+        message: 'For some reason we have not been able to register your user, try again later 🤯',
+        status: 409
+    },
+
+    'invalid token': {
+        message: 'Oops it looks like the entered token is incorrect 😟',
+        status: 403
+    },
+
+    'course already exist': {
+        message: 'Oops it seems that a course with that name already exists 😥',
+        status: 409
+    },
+
+    'course doesnt exist': {
+        message: 'We couldn\'t find the course you requested 😧',
+        status: 404
+    },
+
+    'course wasnt created': {
+        message: 'The course could not be created for some reason, please try again 🙊',
+        status: 417
+    },
+
+    'user not authorized': {
+        message: 'You do not have the necessary permissions to make this request 🤨',
+        status: 401
+    },
+
+    'user is already suscribed': {
+        message: 'You are already suscribed to this course. 😡',
+        status: 406
+    },
+
+    'user isnt suscribed': {
+        message: 'You arent suscribed to this course 💀',
+        status: 400
+    },
+
+    'server error': {
+        message: 'Oops, looks like the server has a problem 🤒🤕',
+        status: 500
+    }
+}
+
+
 const errorHandler = (error, req, res, next) => {
     console.error(error);
 
-    switch(error.message) {
-        case 'user not found':
-            return res.status(404).json({ message: 'Oops, it seems that there is no user with such information 😞' })
+    const errorDescription = ERRORS[error.message];
+    const theresNoErrorForThisCase = !errorDescription;
 
-        case 'the user already exist':
-            return res.status(409).json({ message: 'Try to put a different password or email 😔' });
+    // Detect if theres any error for this case, if the answer is no send the server error message
+    if (theresNoErrorForThisCase) {
+        const serverProblemError = ERRORS['server error'];
 
-        case 'info doesnt meet the requirements':
-            return res.status(401).json({ message: 'The information passed does not meet the minimum accepted requirements 😕' });
-
-        case 'user wasnt created':
-            return res.status(409).json({ message: 'For some reason we have not been able to register your user, try again later 🤯' });
-    
-        case 'invalid token':
-            return res.status(403).json({ message: 'Oops it looks like the entered token is incorrect 😟' });
-    
-        case 'course already exist':
-            return res.status(409).json({ message: 'Oops it seems that a course with that name already exists 😥' });
-        
-        case 'course doesnt exist':
-            return res.status(404).json({ message: 'We couldn\'t find the course you requested 😧' });
-
-        case 'course wasnt created':
-            return res.status(417).json({ message: 'The course could not be created for some reason, please try again 🙊' });
-    
-        case 'user not authorized':
-            return res.status(401).json({ message: 'You do not have the necessary permissions to make this request 🤨' });
-
-        case 'user is already suscribed':
-            return res.status(406).json({ message: 'You are already suscribed to this course. 😡' })
-
-        case 'user isnt suscribed':
-            return res.status(400).json({ message: 'You arent suscribed to this course 💀' }) 
-
-        default:
-            return res.status(500).json({ message: 'Oops, looks like the server has a problem ' });
+        return res.status(serverProblemError.status).json({ 
+            message: serverProblemError.message, 
+            status: serverProblemError.status 
+        });
     }
+    
+    return res.status(errorDescription.status).json({ 
+        message: errorDescription.message, 
+        status: errorDescription.status 
+    });
 }
 
 export default errorHandler;
