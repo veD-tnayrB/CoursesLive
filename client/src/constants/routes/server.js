2ae28c4057188b8c720fe54c065234cf32e790cd
Bryant <90587911+veD-tnayrB@users.noreply.github.com>
2022-07-28 15:03:10 -0400

    😄 Feature: Added the backend urls in Fronted :)

--- client/src/constants/routes/server.js
+++ client/src/constants/routes/server.js
@@ -1,1 +1,7 @@
-export const GET_COURSES_URL = '/courses';
+// COURSES
+export const COURSES_BASE_URL = '/courses';
+export const CREATE_COURSE_URL = '${COURSES_BASE_URL}/create/';
+export const EDIT_COURSE_URL = courseId => `${COURSES_BASE_URL}/${courseId}/edit`;
+export const REMOVE_COURSE_URL = courseId => `${COURSES_BASE_URL}/${courseId}/remove`;
+export const SUSCRIBE_TO_COURSE_URL = courseId => `${COURSES_BASE_URL}/${courseId}/suscribe`;
+export const UNSUSCRIBE_TO_COURSE_URL = courseId => `${COURSE_BASE_URL}/${courseId}/unsuscribe`;
