9416d3545f6401cb4735f227291d961acac7de66
BryantDev_ <Brayanmc.contacto@gmail.com>
2022-09-26 00:27:50 -0400

    ❤ Feature: Has been added the header component in the home page and established the basic tools to use, variables, vite.config, etc.

---
+++ client/src/components/home/hero/index.jsx
@@ -0,0 +1,16 @@
+import './hero.scss';
+
+function Hero() {
+
+    return (
+        <header className="home-hero">
+            <h2>Become what you want!</h2>
+            <p>With our tools you can become the best version of you, learn with out courses and review your knowledge with out test, be part of our team!</p>
+            <button className="primary-button">
+                Let's start
+            </button>
+        </header>
+    )
+}
+
+export default Hero;
