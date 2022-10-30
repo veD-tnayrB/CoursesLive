da3552ef02a031a75bcee1482395b331b29cd82d
veD-tnayrB <Brayanmc.contacto@gmail.com>
2022-10-30 00:30:34 -0400

    Feature: Has been added the durability video functionality and the volume funcionality, also has been fixed the video size

---
+++ client/src/components/course/first-episode/video/duration-indicator.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function EpisodeDurationIndicator({ played, maxDuration }) {
+    maxDuration = (maxDuration - (maxDuration / 60)).toFixed(2).split('.').join(':');
+    played = (played - (played / 60)).toFixed(2).split('.').join(':');
+
+    return (
+        <div className="volume-indicator">
+            <div><span>{played}</span> / <span>{maxDuration}</span></div>
+        </div>
+    )
+}
